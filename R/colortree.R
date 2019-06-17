09c97a106b878b9566d0fe15fcce1b2c88834941
Zebulun Arendsee <arendsee@iastate.edu>
2019-06-17 06:51:36 -0500

    Initial commit

---
+++ R/colortree.R
@@ -0,0 +1,7 @@
+#' Read a nexus file with color annotations into a treedata object with a color column
+#'
+#' @param trefile path to a nexus file
+#' @export
+read_color_nexus <- function(trefile){
+  # DO STUFF
+}
